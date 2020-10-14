1791422764e9a2df213936421c63b13bb680552f
vpurcel <69978582+vpurcel@users.noreply.github.com>
2020-10-14 02:19:46 -0500

    Update sketch.js
    Remove Merge

--- p5/Unit2/anim1/sketch.js
+++ p5/Unit2/anim1/sketch.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var v = 0;
 
 function setup() {
@@ -22,19 +22,3 @@ function draw() {
   }
 
 }
-=======
-var x = 0;
-
-function setup() {
-  createCanvas(500, 500);
-}
-
-function draw() {
-  
-  background(220);
-  textSize(20);
-  text("I forgot my charger!",x,height/2);
-  x+=5
-  if (x>width)  {x=-50;}
-}
->>>>>>> b2a9ed8eef7187c4afa7d8f35654192b93bdcd1b
