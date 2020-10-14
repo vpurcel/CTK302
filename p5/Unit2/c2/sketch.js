c0b2824c3b0d1b7bddf9eef99f5703b336f3c00b
vpurcel <69978582+vpurcel@users.noreply.github.com>
2020-10-14 04:40:26 -0500

    c2+

--- p5/Unit2/c2/sketch.js
+++ p5/Unit2/c2/sketch.js
@@ -5,12 +5,13 @@ let state = 0;
 let timer = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 textSize(40);
+textAlign(CENTER);
+rectMode(CENTER);
   mic = new p5.AudioIn();
   mic.start();
-}
-
+} //setup
 
 function draw() {
 
@@ -19,27 +20,27 @@ function draw() {
   switch (state) {
     case 0:
       background('orange');
-      text("Please be Quiet", 250, 250);
+      text("Please be Quiet", width/2, height/2);
       if (vol > .08) {
         state = 1;
 
-      }
+      } //if vol
       break;
 
     case 1:
       background('red');
-      text("QUIET DOWN YO!", 250, 250);
+      text("QUIET DOWN YO!", width/2, height/2);
       timer = timer + 1;
 
       if (timer > 5 * 80) {
         state = 0;
         timer = 0;
-      }
+      }//iftimer
 
       break;
-  }
-}
+  }//switchstate
+} //draw
 
 function touchStarted() {
   getAudioContext().resume();
-}
+} //touch
