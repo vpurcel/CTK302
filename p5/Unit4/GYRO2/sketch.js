8d182da0aaa1f7d297e99ebb08e394d455ac2616
vpurcel <69978582+vpurcel@users.noreply.github.com>
2020-12-13 16:59:02 -0600

    Gyro2

---
+++ p5/Unit4/GYRO2/sketch.js
@@ -0,0 +1,151 @@
+// variables needed for gyroscope
+var beta, gamma; // orientation data
+var x = 0; // acceleration data
+var y = 0;
+var z = 0;
+var xPosition = 0;
+var yPosition = 0;
+
+// var bunnyImage;
+var cars = [];
+var frogPos;
+
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+
+  // initialize accelerometer variables
+  alpha = 0;
+  beta = 0;
+  gamma = 0;
+
+  // spawn a bunch of cars
+  for (var i = 0; i < 40; i++) {
+    cars.push(new Car());
+  }
+
+  // initialize the frog's position
+  frogPos = createVector(width / 2, height - 80);
+
+  // load any images you need
+  //bunnyImage = loadImage("assets/bunny.jpg");
+  imageMode(CENTER);
+  rectMode(CENTER);
+  noStroke();
+  imgmask = loadImage("assets/mask.png");
+  imgvirus = loadImage("assets/virus.png");
+  imggame = loadImage("assets/game.png");
+  f4 = loadFont("assets/4.ttf");
+}
+
+function draw() {
+
+  background('#c6f5ff'); // light blue
+  image(imggame, width / 2, height / 2, windowWidth, windowHeight);
+  // the map command !!!!
+  // takes your variable and maps it from range 1 to range 2
+  // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
+  xPosition = map(gamma, -18, 18, 0, width);
+  yPosition = map(beta, 25, 45, 0, height);
+
+
+  // move the frog around the screen
+  push(); // before you use translate, rotate, or scale commands, push and then pop after
+  translate(xPosition, yPosition); // move everything over by x, y
+  //  rotate(radians(alpha)); // using alpha in here so it doesn't feel bad
+
+  // draw the FROG
+  // image(bunnyImage, 0, 0, 500, 500);
+
+  image(imgmask, 100, 100, 150, 150);
+  pop();
+
+
+  // update the frog's position using the accelerometer data
+  frogPos.x = xPosition;
+  frogPos.y = yPosition;
+
+  // iterate through the car loop to move them and see if we need to delete cars
+  for (var i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].drive();
+    if (cars[i].pos.dist(frogPos) < 50) {
+      cars.splice(i, 1);
+    }
+  }
+
+  // MORE DECORATIONS - write that pretty ATK type on top.
+
+  fill('White');
+  textFont(f4)
+  textSize(25);
+  text("Wear your Mask! :)", width / 2, 600, windowWidth - 200, windowHeight - 200);
+
+}
+
+function deviceShaken() {
+  // re-spawn cars
+  cars = []; // clear the array first
+  for (var i = 0; i < 40; i++) {
+    cars.push(new Car());
+  }
+}
+
+
+// HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
+
+// Read in accelerometer data
+window.addEventListener('deviceorientation', function(e) {
+  alpha = e.alpha;
+  beta = e.beta;
+  gamma = e.gamma;
+});
+
+
+// accelerometer Data
+window.addEventListener('devicemotion', function(e) {
+  // get accelerometer values
+  x = e.acceleration.x;
+  y = e.acceleration.y;
+  z = e.acceleration.z;
+});
+
+
+
+
+
+// car class!!
+function Car() {
+  // attributes
+  this.pos = createVector(random(width), random(height));
+
+  this.vel = createVector(random(-1, 3), random(-3, 1));
+
+
+  this.r = random(255);
+  this.g = random(255);
+  this.b = random(255);
+  this.a = random(255); // alpha opacity value for fill!
+
+
+  // methods
+  this.display = function() {
+
+    fill(this.r, this.g, this.b, this.a);
+    ellipse(this.pos.x, this.pos.y, 60, 60);
+    image(imgvirus, this.pos.x, this.pos.y, random(20, 30), random(20, 30));
+
+  }
+
+  this.drive = function() {
+    this.pos.add(this.vel);
+
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height;
+
+  }
+
+}
