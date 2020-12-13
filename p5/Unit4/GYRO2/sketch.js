06cb6ae3306dd106ca861fc5319eaf3e2339d343
vpurcel <69978582+vpurcel@users.noreply.github.com>
2020-12-13 17:18:38 -0600

    position modify2

--- p5/Unit4/GYRO2/sketch.js
+++ p5/Unit4/GYRO2/sketch.js
@@ -80,7 +80,7 @@ function draw() {
   fill('White');
   textFont(f4)
   textSize(25);
-  text("Wear your Mask! :)", width / 2, 600, windowWidth - 100, windowHeight - 300);
+  text("Wear your Mask! :)", width / 2, 600, windowWidth/3, windowHeight/3);
 
 }
 
